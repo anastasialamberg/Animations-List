d3e284282e18f6acf5cbe81364f409b7170c7073
anastasialamberg <anastasia.lamberg@gmail.com>
2024-04-11 00:00:27 +0300

    Created test classes

---
+++ src/test/java/hh/sof3/animationlist/AnimationRepositoryTest.java
@@ -0,0 +1,94 @@
+package hh.sof3.animationlist;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import hh.sof3.animationlist.domain.Animation;
+import hh.sof3.animationlist.domain.AnimationRepository;
+import hh.sof3.animationlist.domain.Genre;
+import hh.sof3.animationlist.domain.GenreRepository;
+import hh.sof3.animationlist.domain.Studio;
+import hh.sof3.animationlist.domain.StudioRepository;
+import hh.sof3.animationlist.domain.User;
+import hh.sof3.animationlist.domain.UserRepository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+public class AnimationRepositoryTest {
+
+    @Autowired
+    private AnimationRepository animationRepository;
+
+    @Autowired
+    private StudioRepository studioRepository;
+
+    @Autowired
+    private GenreRepository genreRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void testAnimationRepository() {
+
+        Animation animation = new Animation((long) 3, "Lion King", 1994, "sfjdhdh");
+        animationRepository.save(animation);
+
+        Optional<Animation> foundAnimation = animationRepository.findById((long) 3);
+        assertThat(foundAnimation).isNotEmpty();
+
+        animationRepository.delete(animation);
+        assertThat(animationRepository.findById((long) 3)).isEmpty();
+    }
+
+    @Test
+    void testStudioRepository() {
+        Studio studio = new Studio(((long) 3), "Dreamworks");
+        studioRepository.save(studio);
+
+        Optional<Studio> foundStudios = studioRepository.findById((long) 3);
+        assertThat(foundStudios).isNotEmpty();
+
+        studioRepository.delete(studio);
+
+        assertThat(studioRepository.findById((long) 3)).isEmpty();
+    }
+
+    @Test
+    void testGenreRepository() {
+        Genre genre = new Genre(((long) 3), "Comedy");
+        genreRepository.save(genre);
+
+        Optional<Genre> foundGenre = genreRepository.findById((long) 3);
+        assertThat(foundGenre).isNotEmpty();
+
+        genreRepository.delete(genre);
+
+        assertThat(genreRepository.findById((long) 3)).isEmpty();
+    }
+
+    @Test
+    void testUserRepository() {
+        User user = new User("maijap", "$2a$10$EFPDcnDkAlTQdSQbBCwa9uWo3xdRh4SaAeKuY7g9mTPIBhliT7SJ6",
+                "maijap@gmail.com", "USER");
+        userRepository.save(user);
+
+        User foundUser = userRepository.findByUsername("maijap");
+        assertThat(foundUser).isNotNull();
+        assertThat(foundUser.getUsername()).isNotEmpty();
+
+        userRepository.delete(user);
+
+        assertThat(userRepository.findByUsername("maijap")).isNull();
+    }
+
+}
--- src/test/java/hh/sof3/animationlist/AnimationlistApplicationTests.java
+++ src/test/java/hh/sof3/animationlist/AnimationlistApplicationTests.java
@@ -1,13 +1,41 @@
 package hh.sof3.animationlist;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import hh.sof3.animationlist.web.AnimationRestController;
+import hh.sof3.animationlist.web.animationController;
+import hh.sof3.animationlist.web.genreController;
+import hh.sof3.animationlist.web.studioController;
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class AnimationlistApplicationTests {
 
+	@Autowired
+	private animationController AnimationController;
+
+	@Autowired
+	private studioController studioController;
+
+	@Autowired
+	private genreController genreController;
+
+	@Autowired
+	private AnimationRestController animationRestController;
+
 	@Test
 	void contextLoads() {
+
+		assertThat(AnimationController).isNotNull();
+		assertThat(studioController).isNotNull();
+		assertThat(genreController).isNotNull();
+		assertThat(animationRestController).isNotNull();
 	}
 
 }
