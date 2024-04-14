3e75fbef03171c226b86a0670afddfd89b524137
anastasialamberg <anastasia.lamberg@gmail.com>
2024-04-14 20:13:24 +0300

    Finishing templates

--- src/main/java/hh/sof3/animationlist/WebSecurityConfig.java
+++ src/main/java/hh/sof3/animationlist/WebSecurityConfig.java
@@ -18,35 +18,35 @@ import static org.springframework.security.web.util.matcher.AntPathRequestMatche
 @EnableMethodSecurity(securedEnabled = true)
 public class WebSecurityConfig {
 
-    @Autowired
-    private UserDetailsService userDetailsService;
+        @Autowired
+        private UserDetailsService userDetailsService;
 
-    @Bean
-    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(antMatcher("/css/**")).permitAll()
-                        .requestMatchers(antMatcher("/signup")).permitAll()
-                        .requestMatchers(antMatcher("/saveuser")).permitAll()
-                        .requestMatchers(toH2Console()).permitAll()
-                        .anyRequest().authenticated())
+        @Bean
+        public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+                http
+                                .authorizeHttpRequests(authorize -> authorize
+                                                .requestMatchers(antMatcher("/css/**")).permitAll()
+                                                .requestMatchers(antMatcher("/signup")).permitAll()
+                                                .requestMatchers(antMatcher("/saveuser")).permitAll()
+                                                .requestMatchers(toH2Console()).permitAll()
+                                                .anyRequest().authenticated())
 
-                .csrf(csrf -> csrf
-                        .ignoringRequestMatchers(toH2Console()))
-                .headers(headers -> headers
-                        .frameOptions(frameoptions -> frameoptions
-                                .disable()))
-                .formLogin(formlogin -> formlogin
-                        .loginPage("/login")
-                        .defaultSuccessUrl("/animations", true)
-                        .permitAll())
-                .logout(logout -> logout
-                        .permitAll());
-        return http.build();
-    }
+                                .csrf(csrf -> csrf
+                                                .ignoringRequestMatchers(toH2Console()))
+                                .headers(headers -> headers
+                                                .frameOptions(frameoptions -> frameoptions
+                                                                .disable()))
+                                .formLogin(formlogin -> formlogin
+                                                .loginPage("/login")
+                                                .defaultSuccessUrl("/animations", true)
+                                                .permitAll())
+                                .logout(logout -> logout
+                                                .permitAll());
+                return http.build();
+        }
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
-    }
+        @Autowired
+        public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+                auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+        }
 }
